5759c63130a01fbb06d4ba4acb88e793e00b55af
HwanLin <64071238+HwanLin@users.noreply.github.com>
2020-04-28 12:58:16 +0800

    Add files via upload

---
+++ Pra.java
@@ -0,0 +1,12 @@
+class A{
+	public A() {System.out.print("A");}
+}
+class B extends A{
+	public B() {System.out.print("B");}
+	public static void main(String[]s) {
+		new B();
+	}
+}
+public class Pra {
+
+}
